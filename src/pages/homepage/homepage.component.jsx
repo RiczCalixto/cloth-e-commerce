9c85ace650785f3ed782e6b14158678ad9e5a30c
RiczCalixto <rcalixtos@gmail.com>
2019-11-30 18:57:06 -0300

    menu-item to component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./homepage.style.scss";
+
+export const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <MenuItem />
+    </div>
+  </div>
+);

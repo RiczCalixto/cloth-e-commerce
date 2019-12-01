d9e5caf26d4f389af7f4ff95accb661534be552c
Ric <rcalixtos@gmail.com>
2019-12-01 18:25:20 -0300

    Add Directory and MenuItem components

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import "./homepage.style.scss";
+import { Directory } from "../../components/directory/directory.component";
 
 export const HomePage = () => (
   <div className="homepage">
-    <div className="directory-menu">
-      <MenuItem />
-    </div>
+    <Directory />
   </div>
 );
